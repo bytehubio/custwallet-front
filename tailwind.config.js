68037579c15e8662caf5de9634086dfb4efeadb0
Kirill Shevchenko <kirills167@gmail.com>
2024-10-31 23:29:33 +0000

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,13 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
