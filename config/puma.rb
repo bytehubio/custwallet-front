39e94f8862f724cdaa0b6de9f7afc06d67ba0c8d
Nerzh <emptystamp@gmail.com>
2025-07-31 14:44:19 +0000

    update puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,42 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+workers 1
+threads 2, 2
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
-threads threads_count, threads_count
+app_name = "custwallet.app"
+app_dir = "/home/devton/sites/#{app_name}"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch('PORT', 3000)
+shared_dir = "#{app_dir}/shared"
+pids_folder = "#{shared_dir}/puma"
+log_folder = "#{pids_folder}/log"
+rails_env = ENV['RAILS_ENV'] || "production"
+begin
+  `mkdir #{shared_dir}`
+  `mkdir #{pids_folder}`
+  `mkdir #{log_folder}`
+rescue => ex
+  p "#{ex.message}"
+end
 
-# Allow puma to be restarted by `bin/rails restart` command.
+pidfile "#{pids_folder}/puma.pid"
+state_path "#{pids_folder}/puma.state"
 plugin :tmp_restart
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV['PIDFILE'] if ENV['PIDFILE']
+bind "unix://#{pids_folder}/puma.sock"
+environment rails_env
+
+# NO DATABASE YET
+# on_worker_boot do
+#   require "active_record"
+#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+# end
+
+if rails_env == 'production'
+  stdout_redirect "#{log_folder}/puma.stdout.log", "#{log_folder}/puma.stderr.log", true
+  daemonize false
+  preload_app!
+end
+
+if ENV['RAILS_LOG_TO_STDOUT']
+  stdout_redirect false
+  daemonize false
+end
